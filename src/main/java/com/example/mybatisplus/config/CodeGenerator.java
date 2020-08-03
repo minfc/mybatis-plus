04eb607ffe5fb2035ddbc14152075df5319c7964
minfc <minfc@chinaunicom.cn>
2020-08-03 17:25:18 +0800

    修改项目

--- src/main/java/com/example/mybatisplus/config/CodeGenerator.java
+++ src/main/java/com/example/mybatisplus/config/CodeGenerator.java
@@ -1,4 +1,4 @@
-package com.example.mybatisplus;
+package com.example.mybatisplus.config;
 
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
